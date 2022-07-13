254914208ec66e64d9ad2b9dfaf1488d3942f818
Samuel Pollard <spolla@sandia.gov>
2022-07-13 15:54:18 -0700

    updated sum

--- coq/sum.c
+++ coq/sum.c
@@ -1,10 +1,13 @@
-/*@ requires \valid_read(x);
-  @ requires \is_finite(x); forall 0<=i<=n;
-  @ requires 0.0 <= k <= DBL_MAX / (double) (n+1);
-  @ requires -k <= x[i] <= k forall i, k
-  @ let true_acc = real_sum(x);
-  @ ensures \fp_error(acc, true_acc) <= n*k*eps;
+/*@ requires \valid_read(x + (0 .. n));
+  @ requires \is_finite(k) && k >= 0.0;
+  @ requires \forall integer i; 0 <= i <= n ==>
+  @     \is_finite(x[i]) && -k <= x[i] <= k;
+  @ ensures -k*n <= \exact(acc) <= k*n;
  */
+//   requires 0.0 <= k <= DBL_MAX / (double) (n+1);
+//   requires -k <= x[i] <= k forall i, k
+//   let true_acc = real_sum(x);
+//   ensures \fp_error(acc, true_acc) <= n*k*eps;
 double sum (double *x, int n, double k)
 {
   int i;
