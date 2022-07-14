d064e3806ab80d01973159e76ebb1f27d27b03bf
Mohit Tekriwal <tmohit@citabria.local>
2022-07-14 15:17:29 -0400

    added some annotations

--- coq/sum.c
+++ coq/sum.c
@@ -1,9 +1,15 @@
-/*@ requires \valid_read(x + (0 .. n));
+#include<limits.h>
+
+/*@ requires \valid_read(x + (0 .. n-1));
   @ requires \is_finite(k) && k >= 0.0;
-  @ requires \forall integer i; 0 <= i <= n ==>
+  @ requires \forall integer i; 0 <= i < n ==>
   @     \is_finite(x[i]) && -k <= x[i] <= k;
+  @ requires INT_MIN <= n && n <= INT_MAX;
+  @ assigns \nothing
+
   @ ensures -k*n <= \exact(acc) <= k*n;
  */
+
 //   requires 0.0 <= k <= DBL_MAX / (double) (n+1);
 //   requires -k <= x[i] <= k forall i, k
 //   let true_acc = real_sum(x);
@@ -12,6 +18,11 @@ double sum (double *x, int n, double k)
 {
   int i;
   double acc = 0.0;
+  /*@
+    loop invariant 0 <= i < n;
+    loop assigns i, acc;
+    loop variant n-i;
+  */
   for (i = 0; i < n; i++) {
     acc += x[i];
   }
