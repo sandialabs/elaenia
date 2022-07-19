aabfc28cf4b8482c4034de05a4c73565b8ca403d
Samuel Pollard <spolla@sandia.gov>
2022-07-19 11:32:58 -0700

    Refined sum spec

--- coq/sum.c
+++ coq/sum.c
@@ -1,19 +1,32 @@
 #include<limits.h>
 
+/* Some notes for this spec:
+ * Frama-C converts every float into reals. Next step is to determine whether
+ *   the \exact and the \result actually differ using Frama-C's float model.
+ * max double: 1.8e308
+ * max int: 2.1e9
+ * k <=~ 8.5e298, let's underapproximate to simplify
+ * Make k whatever to make math easier
+ * x      x + u/2    x + u
+ * |---------|---------|
+ * <----* round to nearest
+ * Use 0x1p52 because we do not assume rounding mode
+ */
+
 /*@ requires \valid_read(x + (0 .. n-1));
-  @ requires \is_finite(k) && k >= 0.0;
+  @ requires \is_finite(k) && 0.0 <= k && k <= 1e200;
   @ requires \forall integer i; 0 <= i < n ==>
-  @     \is_finite(x[i]) && -k <= x[i] <= k;
+  @     \is_finite(x[i]) &&
+  @     -k <= x[i] <= k  &&
+  @     \round_error(x[i]) == 0.0;
   @ requires 0 < n && n <= INT_MAX;
   @ assigns \nothing;
 
-  @ ensures -k*n <= \exact(\result) <= k*n;
+  @ ensures \is_finite(\result);
+  @ ensures \result == \sum(0,n-1,\lambda integer i; x[i]);
+  @ ensures \abs(\exact(\result) - \result) <= k*n*0x1p-52
  */
 
-//   requires 0.0 <= k <= DBL_MAX / (double) (n+1);
-//   requires -k <= x[i] <= k forall i, k
-//   let true_acc = real_sum(x);
-//   ensures \fp_error(acc, true_acc) <= n*k*eps;
 double sum (double *x, int n, double k)
 {
   int i;
