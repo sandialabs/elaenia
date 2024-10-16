7b2c04d3a20b53f8b0ffe89a54717fb2ab1313de
Samuel Pollard <spolla@sandia.gov>
2024-10-16 09:57:58 -0700

    proved my first floating point program in frama-c lol

--- examples/01_straightline/01_01_twice.c
+++ examples/01_straightline/01_01_twice.c
@@ -1,8 +1,10 @@
 /* Trying to use the gappa backend for interval arithmetic */
 
-/*@ requires 0.0 <= x && x <= 100.0;
-  @ assigns \nothing;
-  @ ensures \result <= 0.0 && \result <= 200.0;
+/*@ requires \is_finite(x);
+    requires 0.0 <= x && x <= 100.0;
+    assigns \nothing;
+    ensures finite: \is_finite(\result);
+    ensures bound: \result <= 0.0 && \result <= 200.0;
 */
 double twice(double x)
 {
