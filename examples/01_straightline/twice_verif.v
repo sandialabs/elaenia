5835891c6cb4f97b8d74675f3279f6fff22ca602
Sam Pollard <sam.d.pollard@gmail.com>
2025-05-04 16:43:37 -0400

    Got further in the proof, still incomplete

--- examples/01_straightline/twice_verif.v
+++ examples/01_straightline/twice_verif.v
@@ -4,13 +4,47 @@ Require Import vcfloat.FPCompCert.
 Require Import twice.
 
 #[export] Existing Instance NullExtension.Espec.
+#[export] Instance CompSpecs : compspecs. make_compspecs prog. Defined.
+Definition Vprog : varspecs. mk_varspecs prog. Defined.
+
+Local Open Scope float64_scope. 
+
+(* If you don't open the scope, you have to do things like
+  Float.of_int (Int.repr 2) *)
+Definition Twice (x : ftype Tdouble) : ftype Tdouble := Float.mul 2 x.
+
+Definition twice_spec :=
+ DECLARE _twice
+ WITH  x : ftype Tdouble
+ PRE [ tdouble ] PROP() PARAMS(Vfloat x) SEP()
+ POST [ tdouble ] PROP() RETURN (Vfloat (Twice x)) SEP().
+
+Definition main_spec :=
+ DECLARE _main
+  WITH gv: globals
+  PRE  [] main_pre prog tt gv
+  POST [ tint ]
+       PROP() RETURN (Vint (Int.repr 0)) SEP(TT).
+
+Definition Gprog : funspecs := [twice_spec; main_spec].
+
+Lemma body_twice: semax_body Vprog Gprog f_twice twice_spec.
+Proof.
+start_function.
+forward.
+Admitted.
+
+Lemma body_main: semax_body Vprog Gprog f_main main_spec.
+Proof.
+start_function.
+forward_call.
+forward.
+Qed.
 
 Lemma prog_correct:
   semax_prog prog tt Vprog Gprog.
 Proof.
   prove_semax_prog.
-  semax_func_cons body_force.
-  semax_func_cons body_lfstep.
-  semax_func_cons body_integrate.
+  semax_func_cons body_twice.
   semax_func_cons body_main.
 Qed.
